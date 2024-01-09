8a925efeba13311da9c60bec54d177a0f4f9e18c
ryhara <ryosei.hara@gmail.com>
2024-01-10 00:15:14 +0900

    pdfを読み込んで1ページ目のみテキストに変換

--- flask_app/views/chat.py
+++ flask_app/views/chat.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, current_app
 import os
 import openai
+from PyPDF2 import PdfReader
 chat_bp = Blueprint('chat', __name__)
 
 
@@ -10,12 +11,22 @@ def chat():
 
 @chat_bp.route('/send_message', methods=['POST'])
 def send_message():
+    # user_message かpdf_fileどちらかは必須で入る。
+    # pdf_fileは最終的にtextに変換される。
+    # user_messageがない場合はpdf_fileが入っているので、デフォルトのメッセージを入れる。
     user_message = request.form.get('message')
+    if user_message is None:
+        # TODO : デフォルトのメッセージはいいものを考える（英語）。
+        user_message = 'この文章について、解説してください'
+    pdf_file = request.files.get('pdf')
+    text = ''
+    if pdf_file and pdf_file.filename.endswith('.pdf'):
+        reader = PdfReader(pdf_file)
+        text = reader.pages[0].extract_text()
     AZURE_OPENAI_API_KEY = current_app.config['AZURE_OPENAI_API_KEY']
     AZURE_OPENAI_ENDPOINT = current_app.config['AZURE_OPENAI_ENDPOINT']
     os.environ["OPENAI_API_KEY"] = AZURE_OPENAI_API_KEY
     os.environ["OPENAI_ENDPOINT"] = AZURE_OPENAI_ENDPOINT
-    
     openai.api_key = os.environ.get('OPENAI_API_KEY')
     openai.api_type = "azure"
     openai.api_base = os.environ.get('OPENAI_ENDPOINT')
