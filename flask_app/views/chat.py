28455cab6130a6eff59c33ebd50ed0b38a624a07
ryhara <ryosei.hara@gmail.com>
2024-01-09 20:07:09 +0900

    [Fix] api

--- flask_app/views/chat.py
+++ flask_app/views/chat.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, current_app
+from openai import AzureOpenAI
 
 chat_bp = Blueprint('chat', __name__)
 
@@ -12,4 +13,21 @@ def send_message():
     AZURE_OPENAI_API_KEY = current_app.config['AZURE_OPENAI_API_KEY']
     AZURE_OPENAI_ENDPOINT = current_app.config['AZURE_OPENAI_ENDPOINT']
     # TODO : APIを叩いて応答を生成する
-    return "Hello"
+    # client = AzureOpenAI(
+    #     api_version = "2023-12-01-preview",
+    #     api_key = AZURE_OPENAI_API_KEY,
+    #     azure_endpoint = AZURE_OPENAI_ENDPOINT
+    # )
+
+    # completion = client.chat.completions.create(
+    #     model="GPT35TURBO16K",
+    #     messages=[
+    #         {
+    #             "role": "user",
+    #             "content": user_message,
+    #         },
+    #     ],
+    # )
+    # response_message = completion.choices[0].message.content
+    response_message = "Hello World!"
+    return response_message
