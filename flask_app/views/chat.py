5e0167f40a123d35a54cfa8d489d8e03cd036743
ryhara <ryosei.hara@gmail.com>
2024-01-09 14:47:26 +0900

    [Fix] apikey, endpoint

--- flask_app/views/chat.py
+++ flask_app/views/chat.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, current_app
+import os
 
 chat_bp = Blueprint('chat', __name__)
+API_KEY = os.getenv('API_KEY')
 
 @chat_bp.route('/chat')
 def chat():
@@ -9,5 +11,7 @@ def chat():
 @chat_bp.route('/send_message', methods=['POST'])
 def send_message():
     user_message = request.form.get('message')
+    API_KEY = current_app.config['API_KEY']
+    ENDPOINT = current_app.config['ENDPOINT']
     # TODO : APIを叩いて応答を生成する
     return "Hello"
