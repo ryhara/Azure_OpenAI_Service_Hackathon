ddbded6d252c65db9f52ab11fe11eefc0178893a
hotatetabetai <153081735+hotatetabetai@users.noreply.github.com>
2024-01-11 22:18:09 +0900

    Update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -11,11 +11,8 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True,
                 static_folder='./static', template_folder='./templates')
     app.config.from_pyfile('config.py')
-<<<<<<< HEAD
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/pdfs.db'
-=======
     app.config['UPLOAD_FOLDER'] = '/app/flask_app/uploads/'
->>>>>>> 84584f3fa114d2379c84253180cd0a52ec1a84bf
 
     # Database
     db.init_app(app)
