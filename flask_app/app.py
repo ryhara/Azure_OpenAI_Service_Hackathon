51d2c0d497242b0087f983ca55794d7331ce3d53
ryhara <ryosei.hara@gmail.com>
2024-01-13 17:00:43 +0900

    デザイン調整、マニュアル作成

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,5 @@
 from flask import render_template
 from flask_app import create_app
-from flask_app import db
-from flask import request
 
 app = create_app()
 
@@ -13,20 +11,21 @@ def home():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-#databaseの更新
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    if file:
-        pdf_data = file.read()
+# TODO : 多分使わない
+# #databaseの更新
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     file = request.files['file']
+#     if file:
+#         pdf_data = file.read()
 
-        new_pdf = PDFStorage.query.get(1) or PDFStorage(pdf_data)
-        new_pdf.pdf_file = pdf_data
-        db.session.add(new_pdf)
-        db.session.commit()
+#         new_pdf = PDFStorage.query.get(1) or PDFStorage(pdf_data)
+#         new_pdf.pdf_file = pdf_data
+#         db.session.add(new_pdf)
+#         db.session.commit()
 
-        return 'File uploaded and saved successfully'
-    return 'No file uploaded'
+#         return 'File uploaded and saved successfully'
+#     return 'No file uploaded'
 
 if __name__ == '__main__':
     app.run()
