1b1ae286319451aac01e60bb68f2164bdab7ce56
ryhara <ryosei.hara@gmail.com>
2024-01-26 00:03:08 +0900

    csvを読み取れるようにする　ローディング追加

--- flask_app/views/feedback.py
+++ flask_app/views/feedback.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, current_app
+import os
 
 feedback_bp = Blueprint('feedback', __name__)
 
@@ -9,6 +10,12 @@ def feedback():
 @feedback_bp.route('/feedback/send_message/gpt-3-5', methods=['POST'])
 def send_message_3_5():
     user_message = request.form.get('message')
+    csv_file = request.files.get('csv')
+
+    if csv_file:
+        return "csv"
+    if user_message:
+        return "message"
     return "Feedback GPT3.5 function is not implemented yet."
 
 @feedback_bp.route('/feedback/send_message/gpt-4', methods=['POST'])
