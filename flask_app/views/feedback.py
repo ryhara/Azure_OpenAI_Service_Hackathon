f2a75830b553d2cb36bd44997d4a7d085b1f4a8c
Shutan0627 <shutan.maron.0627@keio.jp>
2024-01-26 02:00:01 +0900

    feedback.pyの更新

--- flask_app/views/feedback.py
+++ flask_app/views/feedback.py
@@ -1,7 +1,24 @@
 from flask import Blueprint, render_template, redirect, url_for, request, current_app
 import os
+from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain import PromptTemplate
+from langchain.schema import HumanMessage
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.text_splitter import SpacyTextSplitter
+from langchain.vectorstores import Chroma
+
 
 feedback_bp = Blueprint('feedback', __name__)
+embeddings = AzureOpenAIEmbeddings(
+    azure_deployment="ADA",
+    openai_api_version="2023-05-15"
+)
+database = Chroma(
+    persist_directory="/app/instance/lang",
+    embedding_function=embeddings
+)
 
 @feedback_bp.route('/feedback')
 def feedback():
@@ -9,13 +26,73 @@ def feedback():
 
 @feedback_bp.route('/feedback/send_message/gpt-3-5', methods=['POST'])
 def send_message_3_5():
+    ##ここから
+    chat = AzureChatOpenAI(
+        openai_api_version="2023-05-15",
+        azure_deployment="GPT35TURBO",)
+    
     user_message = request.form.get('message')
     csv_file = request.files.get('csv')
-
     if csv_file:
-        return "csv"
-    if user_message:
-        return "message"
+        csv_filename = csv_file.filename()
+        ###CSVをおいておくディレクトリはuploads/csvにしたい ###これであってます？
+        loader = CSVLoader(file_path=("/uploads/csv/" + csv_filename))
+        data = loader.load()
+        text_splitter = SpacyTextSplitter(
+            chunk_size=200,
+            pipeline="en_core_web_sm"
+        )
+        splitted_documents = text_splitter.split_documents(data)
+        ###persist_directoryは/app/instance以下にデータベースとして配置してください
+        database.add_documents(
+            splitted_documents,
+        )
+
+
+    #ここからはユーザ入力がある場合 
+    if user_message:   
+        documents = database.similarity_search(user_message)
+        document_text = ""
+        for document in documents:
+            document_text += document.page_content
+
+        prompt = PromptTemplate(
+            template="""
+            あなたは質問に対して文献を参照して答えを生成するアシスタントです。今からあなたが置かれている状況を
+            説明しますのでそれに従って出力してください。\n
+            ユーザーは生徒です。ユーザーは調べ学習の後にプレゼンテーションを行い成果を発表しました。生徒の発表を受けて
+            他の生徒はその発表のフィードバックをコメントしました。\n
+            発表した生徒はあなたに質問をします。あなたは与えられた文献をもとに生徒の質問に答えてください\n
+            出力は生徒のプレゼンテーションが改善するようなアドバイスの形式で行い、余計な出力はしないでください。
+            また回答は英語で行なってください。\n
+            出力が英語であることはとても重要です。英語で解答ができているかどうか出力を生成したあともう一度確認するようにしてください
+
+            ###文献\n
+            {documents}\n
+            ###質問\n
+            {query}\n
+            ###出力\n
+            """,
+
+            input_variables=[
+                "documents",
+                "query"
+            ]
+        )
+        ###話し方についての改善施策の部分はユーザー入力にしてください
+        result = chat(
+            [
+                HumanMessage(content=prompt.format(documents=document_text,
+                                                        query=user_message))
+            ]
+        )
+        return result
+
+    ###削除する関数。（削除するかはりょうせいにお任せします）
+    #database.delete_collection()
+
+    ##ここまで
+
     return "Feedback GPT3.5 function is not implemented yet."
 
 @feedback_bp.route('/feedback/send_message/gpt-4', methods=['POST'])
--- flask_app/views/image_caption.py
+++ flask_app/views/image_caption.py
@@ -1,116 +1,60 @@
-!pip install openai==1.9.0
-!pip install chromadb==0.3.29
-!pip install langchain
-!pip install langchain_openai
-!pip install spacy
-!python3 -m spacy download en_core_web_sm
-これ見てrequirements.txt書き換えといて
+from openai import AzureOpenAI
+from flask import Blueprint, render_template, redirect, url_for, request, current_app, jsonify, current_app
+from flask_app.models import Image
+import requests
+import base64
 
-import os
-credential = DefaultAzureCredential()
-os.environ["OPENAI_API_TYPE"] = "azure_ad"
-os.environ['OPENAI_API_KEY'] = ""
-os.environ["OPENAI_ENDPOINT"] = ""
-os.environ['OPENAI_API_VERSION'] = '2023-05-15'
+def encode_image(image_path):
+   with open(image_path,"rb") as image_file:
+      return base64.b64encode(image_file.read()).decode('utf-8')
 
-from langchain_openai import AzureChatOpenAI
-from langchain_openai import AzureOpenAIEmbeddings
-from langchain import PromptTemplate
-from langchain.schema import HumanMessage
-from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.text_splitter import SpacyTextSplitter
-from langchain.vectorstores import Chroma
-
-##ここのモデルは4でも構いません。
-chat = AzureChatOpenAI(
-    openai_api_version="2023-05-15",
-    azure_deployment="GPT35TURBO",)
-
-
-prompt = PromptTemplate(
-    template="""
-    あなたは質問に対して文献を参照して答えを生成するアシスタントです。今からあなたが置かれている状況を
-    説明しますのでそれに従って出力してください。\n
-    ユーザーは生徒です。ユーザーは調べ学習の後にプレゼンテーションを行い成果を発表しました。生徒の発表を受けて
-    他の生徒はその発表のフィードバックをコメントしました。\n
-    発表した生徒はあなたに質問をします。あなたは与えられた文献をもとに生徒の質問に答えてください\n
-    出力は生徒のプレゼンテーションが改善するようなアドバイスの形式で行い、余計な出力はしないでください。
-    また回答は英語で行なってください。\n
-    出力が英語であることはとても重要です。英語で解答ができているかどうか出力を生成したあともう一度確認するようにしてください
-
-    ###文献\n
-    {documents}\n
-    ###質問\n
-    {query}\n
-    ###出力\n
-    """,
-
-    input_variables=[
-        "documents",
-        "query"
-    ]
-)
-###CSVをおいておくディレクトリはuploads/csvにしたい
-loader = CSVLoader(file_path="/content/sample_feedback.csv")
-
-data = loader.load()
-
-text_splitter = SpacyTextSplitter(
-    chunk_size=200,
-    pipeline="en_core_web_sm"
-)
-
-splitted_documents = text_splitter.split_documents(data)
-
-embeddings = AzureOpenAIEmbeddings(
-    azure_deployment="ADA",
-    openai_api_version="2023-05-15"
-)
-###persist_directoryは/app/instance以下にデータベースとして配置してください
-database = Chroma(
-    persist_directory="./data",
-    embedding_function=embeddings
-)
-database.add_documents(
-    splitted_documents,
-)
-
-prompt = PromptTemplate(
-    template="""
-    あなたは質問に対して文献を参照して答えを生成するアシスタントです。今からあなたが置かれている状況を
-    説明しますのでそれに従って出力してください。\n
-    ユーザーは生徒です。ユーザーは調べ学習の後にプレゼンテーションを行い成果を発表しました。生徒の発表を受けて
-    他の生徒はその発表のフィードバックをコメントしました。\n
-    発表した生徒はあなたに質問をします。あなたは与えられた文献をもとに生徒の質問に答えてください\n
-    出力は生徒のプレゼンテーションが改善するようなアドバイスの形式で行い、余計な出力はしないでください。
-    また回答は英語で行なってください。\n
-    出力が英語であることはとても重要です。英語で解答ができているかどうか出力を生成したあともう一度確認するようにしてください
-
-    ###文献\n
-    {documents}\n
-    ###質問\n
-    {query}\n
-    ###出力\n
-    """,
-
-    input_variables=[
-        "documents",
-        "query"
-    ]
-)
-###話し方についての改善施策の部分はユーザー入力にしてください
-result = chat(
-    [
-        HumanMessage(content=prompt.format(documents=document_text,
-                                                query="話し方についての改善施策"))
-    ]
-)
+def get_image_caption(image_path):
+    azure_endpoint =  current_app['OPENAI_ENDPOINT']+"/openai/deployments/GPT4/chat/completions?api-version=2023-12-01-preview"
+    api_key = current_app.config['OPENAI_API_KEY']
+    gpt4_turbo_vision = "gpt-4-vision-preview"
+    prompt = "please describe this image."
+    b64_image = encode_image(image_path)
+    client = AzureOpenAI(
+       api_key = api_key,
+       azure_endpoint = azure_endpoint,
+       api_version = "2023-12-01-preview"
+    )
+    response = client.chat.completions.create(
+       model = gpt4_turbo_vision,
+       messages=[
+          {
+             "role":"user",
+             "content":[
+                {
+                   "type":"text",
+                   "text":prompt
+                },
+                {
+                   "type":"image_url",
+                   "image_url":{
+                      "url":f"data:image/jpeg;base64,{b64_image}",
+                      "detail":"low"
+                   }
+                }
+             ]
+          }
+       ],
+       max_tokens=30
+    )
+    image_caption = str(response.choices[0].message.content)
+    return image_caption
 
 
-
-
-
-
-###削除する関数。
-database.delete_collection()
+def text_embedding(input):
+  api_key = current_app.config['OPENAI_API_KEY']
+  azure_endpoint = current_app.config['OPENAI_ENDPOINT']
+  client = AzureOpenAI(
+       api_key = api_key,
+       azure_endpoint = azure_endpoint,
+       api_version = "2023-05-15"
+    )
+  response = client.embeddings.create(
+     model='ADA',
+     input=input
+  )
+  return response.data[0].embedding
